d79a9d26a5ce39e950e055407956a7030287c1b5
Refayat Haque <refayathaque@gmail.com>
2021-12-03 15:05:08 -0500

    updated gitignore

--- provider.tf
+++ provider.tf
@@ -1,11 +1,5 @@
-variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "ExampleAppServerInstance"
-}
-
 provider "google" {
-  project     = var.gcp_project
-  credentials = file(var.gcp_auth_file)
-  region      = var.gcp_region
+  project     = var.project_id
+  credentials = file(var.service_account_key)
+  region      = var.region
 }
