8d952533a2e3e9b527e62a43b2e69e53d1505ac0
Refayat Haque <refayathaque@gmail.com>
2021-12-03 15:42:00 -0500

    git ignore for node modules

--- storage.tf
+++ storage.tf
@@ -1,4 +1,29 @@
-resource "google_storage_bucket" "test" {
-  name          = "test"
-  location      = "US"
+resource "google_storage_bucket" "ferrari_bucket" {
+  name          = "ferrari_bucket"
+  location      = var.region
+}
+
+resource "google_storage_bucket" "mclaren_mercedes" {
+  name          = "mclaren_mercedes"
+  location      = var.region
+}
+
+resource "google_storage_bucket" "red_bull_racing_honda" {
+  name          = "red_bull_racing_honda"
+  location      = var.region
+}
+
+resource "google_storage_bucket" "mercedes_bucket" {
+  name          = "mercedes_bucket"
+  location      = var.region
+}
+
+resource "google_storage_bucket" "alpine_renault" {
+  name          = "alpine_renault"
+  location      = var.region
+}
+
+resource "google_storage_bucket" "alphatauri_honda" {
+  name          = "alphatauri_honda"
+  location      = var.region
 }
