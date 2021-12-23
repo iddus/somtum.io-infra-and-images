932ad12b0054395895dbb4087e617a122a846575
Refayat Haque <refayathaque@gmail.com>
2021-12-23 12:13:38 -0500

    back up

--- artifact_registry.tf
+++ artifact_registry.tf
@@ -3,4 +3,5 @@ resource "google_artifact_registry_repository" "my-repo" {
   location      = var.region
   repository_id = "nodejs-containers"
   format        = "DOCKER"
+  depends_on    = [google_project_service.artifact_registry]
 }
