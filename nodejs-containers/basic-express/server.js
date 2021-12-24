c32ba7691e275f062946202d7e54279a7920754b
Refayat Haque <refayathaque@gmail.com>
2021-12-24 01:00:25 -0500

    helloWorld

--- nodejs-containers/basic-express/server.js
+++ nodejs-containers/basic-express/server.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const app = express();
-const uuidv4 = require("uuid/v4");
-const PORT = process.env.PORT || 8080;
-// set to 8080 instead of 3000 here https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
+const { v4 } = require("uuid");
 const HOST = "0.0.0.0";
-// also calling this file server.js as per ^ doc (https://nodejs.org/en/docs/guides/nodejs-docker-webapp/), express doc calls it app.js, changed this in the package.json as well
+const PORT = process.env.PORT || 8080;
+// node.js doc (https://cloud.google.com/run/docs/reference/container-contract#port) set port to 8080 instead of 3000 like in express.js doc (https://expressjs.com/en/starter/hello-world.html), cloud run doc (https://cloud.google.com/run/docs/reference/container-contract#port) also say port must be 8080
+// also calling this file server.js as per node.js doc (https://nodejs.org/en/docs/guides/nodejs-docker-webapp/), express doc calls it app.js, changed this in the package.json as well
 // "The environment variables defined in the container runtime contract are reserved and cannot be set. In particular, the PORT environment variable is injected inside your container by Cloud Run. You should not set it yourself." - https://cloud.google.com/run/docs/configuring/environment-variables - got port declaration and initialization line from https://cloud.google.com/run/docs/quickstarts/build-and-deploy/nodejs
 // process.env.<?> references an environment variable and can be configured in tf code for the cloud run resource (search for env block in doc) - https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service
 
-const uuid = uuidv4();
-// https://dev.to/rahmanfadhil/how-to-generate-unique-id-in-javascript-1b13
+const uuid = v4();
+// https://www.npmjs.com/package/uuid
 
 app.get("/", (req, res) => {
-  res.send(`hello world! ${uuid}`);
+  res.send(`response from basic-express app / version:${uuid}`);
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-
-// https://expressjs.com/en/starter/hello-world.html
