d090a8c12bffaa49be8151ba6471a8741df4bfb2
Refayat Haque <refayathaque@gmail.com>
2021-12-24 10:50:46 -0500

    back up

--- nodejs-containers/basic-express/server.js
+++ nodejs-containers/basic-express/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const app = express();
 const { v4 } = require("uuid");
+
 const HOST = "0.0.0.0";
 const PORT = process.env.PORT || 8080;
 // node.js doc (https://cloud.google.com/run/docs/reference/container-contract#port) set port to 8080 instead of 3000 like in express.js doc (https://expressjs.com/en/starter/hello-world.html), cloud run doc (https://cloud.google.com/run/docs/reference/container-contract#port) also say port must be 8080
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 8080;
 // "The environment variables defined in the container runtime contract are reserved and cannot be set. In particular, the PORT environment variable is injected inside your container by Cloud Run. You should not set it yourself." - https://cloud.google.com/run/docs/configuring/environment-variables - got port declaration and initialization line from https://cloud.google.com/run/docs/quickstarts/build-and-deploy/nodejs
 // process.env.<?> references an environment variable and can be configured in tf code for the cloud run resource (search for env block in doc) - https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_service
 
+const app = express();
 const uuid = v4();
 // https://www.npmjs.com/package/uuid
 
